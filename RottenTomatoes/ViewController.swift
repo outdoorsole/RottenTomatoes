b935919a57982b07077b73d6a944c1246bdf2348
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-02 23:01:03 -0700

    Initial Commit

---
+++ RottenTomatoes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RottenTomatoes
+//
+//  Created by m on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

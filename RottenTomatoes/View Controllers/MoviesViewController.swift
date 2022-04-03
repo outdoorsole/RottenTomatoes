f9db422090f12af372190ae56c5c249cd55705c4
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-03 09:30:22 -0700

    Create outlet for tableView from Main.storyboard to MoviesViewController

--- RottenTomatoes/View Controllers/MoviesViewController.swift
+++ RottenTomatoes/View Controllers/MoviesViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MoviesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }

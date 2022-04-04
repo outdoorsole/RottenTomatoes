62a14cee2d8356dd4cec3c3b585eab1c86fcff0e
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-03 22:13:30 -0700

    Store the top box office movies results from the JSON

--- RottenTomatoes/View Controllers/MoviesViewController.swift
+++ RottenTomatoes/View Controllers/MoviesViewController.swift
@@ -11,16 +11,22 @@ class MoviesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    var movies: [[String: Any]]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard let url = URL(string: "https://www.rottentomatoes.com/api/private/v2.0/browse?type=in-theaters") else { return }
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+
             if let data = data {
                 do {
-                    let jsonObject = try JSONSerialization.jsonObject(with: data)
-                    print(jsonObject)
+                    let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                    if let json = jsonObject {
+                        self.movies = json["results"] as? [[String: Any]]
+                    }
                 } catch {}
             }
         }

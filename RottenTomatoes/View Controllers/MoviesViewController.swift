0bcf449612b2a6bc61f5a814c3d3ee380370489e
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-03 21:45:44 -0700

    Print jsonObject (for the top box office movies) from the requested data

--- RottenTomatoes/View Controllers/MoviesViewController.swift
+++ RottenTomatoes/View Controllers/MoviesViewController.swift
@@ -13,6 +13,18 @@ class MoviesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        guard let url = URL(string: "https://www.rottentomatoes.com/api/private/v2.0/browse?type=in-theaters") else { return }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: data)
+                    print(jsonObject)
+                } catch {}
+            }
+        }
+        .resume()
     }
 
 }

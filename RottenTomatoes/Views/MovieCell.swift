98b07d7f6c337f507e1bd56e94d44303024bc520
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-04 22:50:56 -0700

    Create MovieCell.swift file

---
+++ RottenTomatoes/Views/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  RottenTomatoes
+//
+//  Created by m on 4/4/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

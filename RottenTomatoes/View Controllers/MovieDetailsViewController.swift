b6e455d1c31612e339cbcaaf93c9001b7954f479
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-08 04:55:18 -0700

    Add property for selected movie in MovieDetailsViewController

--- RottenTomatoes/View Controllers/MovieDetailsViewController.swift
+++ RottenTomatoes/View Controllers/MovieDetailsViewController.swift
@@ -13,6 +13,8 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
 
+    var movie: [String: Any]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }

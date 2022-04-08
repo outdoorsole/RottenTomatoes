612c6dfd0da7bde0c97638f50720468b045799c4
Maribel M <outdoorsole@users.noreply.github.com>
2022-04-07 23:14:59 -0700

    Create MovieDetailsViewController

---
+++ RottenTomatoes/View Controllers/MovieDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailsViewController.swift
+//  RottenTomatoes
+//
+//  Created by m on 4/7/22.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
